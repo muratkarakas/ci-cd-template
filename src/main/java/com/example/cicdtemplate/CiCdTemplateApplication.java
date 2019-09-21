f75da4aeab62ac380dca4b5ae52a3d1334a27924
Murat Karakas <murat.karakas@eteration.com>
2019-09-21 23:33:47 +0300

    initial commit

---
+++ src/main/java/com/example/cicdtemplate/CiCdTemplateApplication.java
@@ -0,0 +1,22 @@
+package com.example.cicdtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+@RestController
+public class CiCdTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTemplateApplication.class, args);
+	}
+
+	@GetMapping(value="/ping")
+	public String getMethodName( ) {
+		return  "pong";
+	}
+	
+}
