5b6315e0a4b44b7ccfab04b131e82bd43fb48e69
Murat Karakas <murat.karakas@eteration.com>
2019-10-04 22:25:16 +0300

    ....

--- src/test/java/com/example/cicdtemplate/CiCdTemplateApplicationTests.java
+++ src/test/java/com/example/cicdtemplate/CiCdTemplateApplicationTests.java
@@ -2,15 +2,22 @@ package com.example.cicdtemplate;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CiCdTemplateApplicationTests {
-
+	@Autowired
+    private MockMvc mockMvc;
 	@Test
-	public void contextLoads() {
+	public void ping() throws Exception {
+		mockMvc.perform(get("/ping")).andExpect(status().isOk()).andExpect(content().string("pong"));
 	}
 
 }
