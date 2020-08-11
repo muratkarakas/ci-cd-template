e5c89c108264421044579051f7beb69e266cee2a
Murat Karakas <murat.karakas@eteration.com>
2020-08-11 11:33:12 +0300

    jenkins basic file ,docker componse file added

--- src/test/java/com/example/cicdtemplate/CiCdTemplateApplicationTests.java
+++ src/test/java/com/example/cicdtemplate/CiCdTemplateApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @RunWith(SpringRunner.class)
@@ -17,7 +18,7 @@ public class CiCdTemplateApplicationTests {
     private MockMvc mockMvc;
 	@Test
 	public void ping() throws Exception {
-		mockMvc.perform(get("/ping")).andExpect(status().isOk()).andExpect(content().string("pong"));
+		mockMvc.perform(get("/ping")).andDo(print()).andExpect(status().isOk()).andExpect(content().string("pong"));
 	}
 
 }
