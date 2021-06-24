463c770a6ab8f2bd05026de551ba75eeeafdd3cc
Murat Karakas <murat.karakas@eteration.com>
2021-06-24 14:28:22 +0300

    more docs

--- src/main/java/com/example/cicdtemplate/CiCdTemplateApplication.java
+++ src/main/java/com/example/cicdtemplate/CiCdTemplateApplication.java
@@ -12,6 +12,7 @@ public class CiCdTemplateApplication {
 
   /**
    * spring-boot main class.
+   * @param  standard main args
    */
   public static void main(String[] args) {
     SpringApplication.run(CiCdTemplateApplication.class, args);
